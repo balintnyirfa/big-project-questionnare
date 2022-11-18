ec390727dfe34fbaaec0933c10dca1c9d824b69e
balintnyirfa <balint.nyirfa@gmail.com>
2022-11-18 20:30:51 +0100

    Navbar frissítve

--- Code/resources/views/layouts/app.blade.php
+++ Code/resources/views/layouts/app.blade.php
@@ -24,25 +24,51 @@
         html{
             height: 100%;
         }
+        @media only screen and (min-width: 1200px){
+            .befogo{
+                width: 75%;
+            }
+        }
+
+        @media only screen and (min-width: 992px){
+            .befogo{
+                width: 75%;
+            }
+        }
+
+        @media only screen and (max-width: 768px){
+            .befogo{
+                width: 100%;
+            }
+        }
+
+        @media only screen and (max-width: 600px){
+            .befogo{
+                width: 100%;
+            }
+        }
     </style>
 </head>
 <body style="background: linear-gradient(150.32deg, #C86FFF 0%, #C586FF 18.62%, #C19DFF 35.17%, #BEB4FF 53.27%, #BACAFF 71.37%, #B7E1FF 84.3%, #B3F8FF 99.3%) !important; background-size: cover; background-repeat: no-repeat;">
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm" style="background-color: rgba(137, 137, 137, 50%) !important; border-radius: 0px 0px 24px 24px;">
-            <div class="container">
-                
+        <nav class="navbar navbar-expand-lg bg-light navbar-dark" style="background-color: rgba(137, 137, 137, 50%) !important; border-radius: 0px 0px 24px 24px;">
+            <div class="container-fluid befogo">
+                <a class="navbar-brand d-lg-none">Testlux</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
+                <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
+                    <ul class="nav navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Valami</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Valami</a>
+                        </li>
                     </ul>
-                    <a class="navbar-brand">Testlux</a>
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
+                    <a class="navbar-brand m-0">Testlux</a>
+                    <ul class="nav navbar-nav">
                         <!-- Authentication Links -->
                         @guest
                             @if (Route::has('login'))
