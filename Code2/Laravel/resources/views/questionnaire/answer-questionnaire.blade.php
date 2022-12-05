b131343d9929a778232d856981f4d8b6e2e04722
balintnyirfa <balint.nyirfa@gmail.com>
2022-12-05 22:59:22 +0100

    Kitöltés design change

--- Code2/Laravel/resources/views/questionnaire/answer-questionnaire.blade.php
+++ Code2/Laravel/resources/views/questionnaire/answer-questionnaire.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
 <head>
-    <title>Answering</title>
+    <title>Válaszadás</title>
 </head>
 
-<div class="d-flex justify-content-center">
+<div class="d-flex mx-auto flex-column justify-content-center" style="width: 500px !important; padding: 15px; background-color: rgba(137, 137, 137, 50%) !important; color: white; border-radius: 24px; width: 230px; margin: 10px;">
 	@livewire('answer-questionare', ['id' => $id])
 </div>
 
